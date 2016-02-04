467b9b3e96397bb0904179718049108b1d5a1231
Jack Anteby <Janteby1@gmail.com>
2016-02-04 12:37:37 -0500

    added welcome statment

---
+++ exercises/terminal_trader/trader_views.py
@@ -0,0 +1,13 @@
+import sys
+import pudb
+
+class View ():
+
+	def welcome (self):
+		print ("")
+		print ("Welcome to the trading game app!")
+		print ("This app allows you to search for a company and get information back about it.")
+		print ("You can search for a stock's live ticker price.")
+		print ("After creating an account with a username and password you can start building your portfolio!")
+		print ("Buy and sell stocks to try and make a profit.")
+		print ("Lets get started ... ")
