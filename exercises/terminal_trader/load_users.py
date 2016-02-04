2aedc5b7771ec502b9276fbffc8e9c598a12da03
sulekha Cheruvu <sulashpawar1@gmail.com>
2016-02-04 14:48:54 -0500

    fixed load programs

--- exercises/terminal_trader/load_users.py
+++ exercises/terminal_trader/load_users.py
@@ -1,27 +1,32 @@
+import sqlite3
+
 class database:
 	def __init__(self):
 		self.dbname = 'stocks.db'
-		self.conn = sqlite3.connect('stocks.db')
+		
 
-	def insert_user_data(self,uname,pwd,balance):
+	def insert_user_data(self,name,uname,pwd,balance):
+		self.conn = sqlite3.connect('stocks.db')
+		self.name=name
 		self.uname=uname
 		self.pwd=pwd
 		self.balance=balance
 
 		self.conn.execute(
 	    """
-	    INSERT INTO users ("username","password","balance") VALUES(?,?,?)
+	    INSERT INTO users ("name","username","password","balance") VALUES(?,?,?,?)
 	    
-	    """,(self.uname,self.pwd,self.balance)
+	    """,(self.name,self.uname,self.pwd,self.balance)
 		)
 
 		self.conn.commit()
-		conn.close()
+		self.conn.close()
 
 
+mydb = database()
 with open('users.csv') as fp:
 		for line in fp:
 			line.rstrip()
 			wrds = line.split(",")
-			mydb.insert_employee_data(wrds[0],wrds[1],wrds[2])
+			mydb.insert_user_data(wrds[0],wrds[1],wrds[2],wrds[3])
 			
