ca05d11a31ec06eec97d5272ab8a05d0df3e0eef
Jack Anteby <Janteby1@gmail.com>
2016-02-05 11:11:25 -0500

    working buy function

--- exercises/terminal_trader/trader_models.py
+++ exercises/terminal_trader/trader_models.py
@@ -7,70 +7,67 @@ connection = sqlite3.connect(db)
 c = connection.cursor()
 
 class Model:
-	# def __init__(self):
-		# self.stock_name=name
-		#self.apiname = 'http://dev.markitondemand.com/MODApis/#doc_lookup'
+    # def __init__(self):
+        # self.stock_name=name
+        #self.apiname = 'http://dev.markitondemand.com/MODApis/#doc_lookup'
 
-	def search_company(self, company):
-		''' get company information'''
-		stock_company = requests.get("http://dev.markitondemand.com/Api/v2/Lookup/json?input=%s" % (company)).json() 
-		return(stock_company)
+    def search_company(self, company):
+        ''' get company information'''
+        stock_company = requests.get("http://dev.markitondemand.com/Api/v2/Lookup/json?input=%s" % (company)).json() 
+        return(stock_company)
 
-	def stock_info(self, stock):
-		stock_info = requests.get("http://dev.markitondemand.com/Api/v2/Quote/json?symbol=%s" %(stock)).json()
-		return stock_info
+    def stock_info(self, stock):
+        stock_info = requests.get("http://dev.markitondemand.com/Api/v2/Quote/json?symbol=%s" %(stock)).json()
+        return stock_info
 
-	def create_user(self, name, username, password):
-		c.execute("""
-	        INSERT INTO users ("name", "username", "password") VALUES (?, ?, ?)""",(name, username, password))
-		connection.commit()
+    def create_user(self, name, username, password):
+        c.execute("""
+            INSERT INTO users ("name", "username", "password") VALUES (?, ?, ?)""",(name, username, password))
+        connection.commit()
 
-	def print_user(self):
-		cursor = connection.execute("SELECT name, username, password, permission FROM users")
-		i =0
-		while i < 1:
-			for row in cursor:
-				print (" ")
-				print (" --- DATABASE ---")
-				print ("Users")
-				print ("NAME = ", row[0])
-				print ("USERNAME = ", row[1])
-				print ("PASSWORD = ", row[2], '\n')
-			i +=1
+    def print_user(self):
+        cursor = connection.execute("SELECT name, username, password, permission FROM users")
+        i =0
+        while i < 1:
+            for row in cursor:
+                print (" ")
+                print (" --- DATABASE ---")
+                print ("Users")
+                print ("NAME = ", row[0])
+                print ("USERNAME = ", row[1])
+                print ("PASSWORD = ", row[2], '\n')
+            i +=1
 
-	def check_login(self, username, password):
-		info_list = []
-		info_list = c.execute("""
-			SELECT * FROM users WHERE username = ? AND password = ?""", (username, password))
-		connection.commit()
-		#return info_list.fetchone()
-		return(info_list.fetchall()) 
+    def check_login(self, username, password):
+        info_list = []
+        info_list = c.execute("""
+            SELECT * FROM users WHERE username = ? AND password = ?""", (username, password))
+        connection.commit()
+        #return info_list.fetchone()
+        return(info_list.fetchall()) 
 
-	def buy_stock(self,stock,lastprice,num,userid,new_balance):
-		self.stock=stock
-		self.lastprice=lastprice
-		self.num=num
-		self.userid=userid
-		self.new_balance=new_balance
-		print('LastPrice  ',self.lastprice, '  ','user id ..',self.userid )
+    def buy_stock(self,stock,lastprice,num,userid,new_balance):
+        "Need to change the value of the balance in the user table then create the stock add the stock to his FK"
+        self.stock=stock
+        self.lastprice=lastprice
+        self.num=num
+        self.userid=userid
+        self.new_balance=new_balance
+        print('LastPrice  ',self.lastprice, '  ','user id ..',self.userid )
 
-		c.execute("""
-			INSERT INTO stock ("stockName","buyPrice","num","userid") VALUES(?,?,?,?)
-	    
-	    """,(self.stock,self.lastprice,self.num,self.userid)
-		)
+        c.execute("""
+            INSERT INTO stock ("stockName","buyPrice","num","userid") VALUES(?,?,?,?)
+        
+        """,(self.stock,self.lastprice,self.num,self.userid)
+        )
 
-		c.execute("""
-			UPDATE users SET balance=? WHERE id=?""",(self.new_balance, self.userid)
-		)
-		connection.commit()
-		connection.close()
-
-	def buy_stock(self, userid, acttype, balance):
-		"Need to change the value of the balance in the user table then create the stock add the stock to his FK"
-		pass
-	
-	def sell_stock(self, userid, stock_name, quantity):
+        c.execute("""
+            UPDATE users SET balance=? WHERE id=?""",(self.new_balance, self.userid)
+        )
+        connection.commit()
+        connection.close()
+    
+    def sell_stock(self, userid, stock_name, quantity):
         c.execute(
             """
             SELECT id, buyPrice, num
@@ -106,14 +103,14 @@ class Model:
         connection.commit()
         return revenue
 
-	def get_portfolio(self, name, username):
-		"first fine user id from his name and username, then use this id to find all the stocks he has"
-		portfolio = []
-		c.execute("""
-			SELECT * FROM stock where userid =(SELECT id FROM user WHERE name = ? AND username = ?"""), (name, username))
-		return portfolio.fetchall() 
-		connection.commit()
+    def get_portfolio(self, name, username):
+        "first fine user id from his name and username, then use this id to find all the stocks he has"
+        portfolio = []
+        c.execute("""
+            SELECT * FROM stock where userid =(SELECT id FROM user WHERE name = ? AND username = ?)"""), (name, username)
+        return portfolio.fetchall() 
+        connection.commit()
 
-	def admin_view(self, userid, acttype, balance):
-		"Need to select all stocks from all users, ordered by user name"
-		pass
+    def admin_view(self, userid, acttype, balance):
+        "Need to select all stocks from all users, ordered by user name"
+        pass
