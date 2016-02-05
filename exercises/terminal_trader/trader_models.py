49fca328f9e8e834a35ba2508752ec5529a8a250
Shih Mei Kong <shihmeikong@gmail.com>
2016-02-05 00:29:48 -0500

    Add sell stock option and made minor grammer changes in view

---
+++ exercises/terminal_trader/trader_models.py
@@ -0,0 +1,109 @@
+<<<<<<< refs/remotes/origin/shihmei
+=======
+import sqlite3
+import requests
+import pdb
+
+db = "stocks.db"
+connection = sqlite3.connect(db)
+c = connection.cursor()
+
+class Model:
+	# def __init__(self):
+		# self.stock_name=name
+		#self.apiname = 'http://dev.markitondemand.com/MODApis/#doc_lookup'
+
+	def search_company(self, company):
+		''' get company information'''
+		stock_company = requests.get("http://dev.markitondemand.com/Api/v2/Lookup/json?input=%s" % (company)).json() 
+		return(stock_company)
+
+	def stock_info(self, stock):
+		stock_info = requests.get("http://dev.markitondemand.com/Api/v2/Quote/json?symbol=%s" %(stock)).json()
+		return stock_info
+
+	def create_user(self, name, username, password):
+		c.execute("""
+	        INSERT INTO users ("name", "username", "password") VALUES (?, ?, ?)""",(name, username, password))
+		connection.commit()
+
+	def print_user(self):
+		cursor = connection.execute("SELECT name, username, password, permission FROM users")
+		i =0
+		while i < 1:
+			for row in cursor:
+				print (" ")
+				print (" --- DATABASE ---")
+				print ("Users")
+				print ("NAME = ", row[0])
+				print ("USERNAME = ", row[1])
+				print ("PASSWORD = ", row[2], '\n')
+			i +=1
+
+	def check_login(self, username, password):
+		info_list = []
+		info_list = c.execute("""
+			SELECT id FROM users WHERE username = ? AND password = ?""", (username, password))
+		connection.commit()
+		return info_list.fetchone() 
+
+	def buy_stock(self, userid, acttype, balance):
+		"Need to change the value of the balance in the user table then create the stock add the stock to his FK"
+		pass
+	
+	def sell_stock(self, userid, stock_name, quantity):
+        c.execute(
+            """
+            SELECT id, buyPrice, num
+            FROM stock
+            WHERE stockname = ? AND userid = ?""",
+            (stockName, userid))
+        available_stock = c.fetchone()
+        if available_stock == []:
+            print("You do not have this stock")
+        stock_id, price, on_hand = available_stock
+        if quantity > on_hand:
+            print("Not enough stock available!")
+            return
+
+        c.execute(
+            """
+            UPDATE stock
+            SET num = num - ?
+            WHERE id = ?""",
+            (quanity, stock_id))
+        connection.commit()
+
+        revenue = price * quality
+        print("Stock sell sucessesful."
+              "You have gained {}".format(revenue))
+
+        c.execute(
+            """
+            UPDATE users
+            SET balance = balance + ?
+            WHERE id = ?""",
+            (revenue, userid))
+        connection.commit()
+        return revenue
+
+	def get_portfolio(self, name, username):
+		"first fine user id from his name and username, then use this id to find all the stocks he has"
+		portfolio = []
+		c.execute("""
+			SELECT * FROM stock where userid =(SELECT id FROM user WHERE name = ? AND username = ?"""), (name, username))
+		return portfolio.fetchall() 
+		connection.commit()
+
+	def admin_view(self, userid, acttype, balance):
+		"Need to select all stocks from all users, ordered by user name"
+		pass
+
+
+
+
+
+
+
+	
+>>>>>>> local
--- exercises/terminal_trader/trader_views.py
+++ exercises/terminal_trader/trader_views.py
@@ -22,3 +22,99 @@ class View ():
 				self.get_name()
 		return name
 
+<<<<<<< refs/remotes/origin/shihmei
+=======
+	def company_info(self,name,exchange,symbol):
+		self.name=name
+		self.exchange=exchange
+		self.symbol = symbol
+		print("")
+		print('Name of the company: {}'.format(self.name))
+		print('Exchange: {}'.format(self.exchange))
+		print('Symbol: {}'.format(self.symbol))
+
+	def get_username (self):
+		print ("")
+		while True:
+			username = input ("Please enter a username: ")
+			if username.isalpha() == True:
+				break
+			else:
+				self.get_username()
+		return username
+
+	def get_password (self):
+		print ("")
+		while True:
+			password = input ("Choose a password: ")
+			if password.isalpha() == True:
+				break
+			else:
+				self.get_password()
+		return password
+
+	def print_signup (self, name, username):
+		print ("")
+		print ("Welcome ", name)
+		print ("You are currently logged in as: ", username)
+
+	def login_username(self):
+		print ("")
+		self.name = input ("What is your name: ")
+		self.username = input ("Please enter your username: ")
+		return self.username
+
+	def login_password(self):
+		self.password = input ("Enter your password: ")
+		return self.password
+
+	def try_again (self):
+		print ("We coult not find your username or password, please try again")
+
+	def restart(self):
+		print("")
+		print ("Great! You created an account.")
+		print ("Please restart and log back in.")
+		print ("Thank you")
+
+	def get_company (self):
+		# need to fix this input
+		print ("")
+		while True:
+			company = input ("Please enter a company: ")
+			if company.isalpha() == True:
+				break
+			else:
+				self.get_company()
+		return company
+
+	def get_stock (self):
+		print ("")
+		while True:
+			stock = input ("Enter the companies stock symbol: ")
+			if stock.isalpha() == True:
+				break
+			else:
+				self.get_stock()
+		return stock
+
+	def stock_quote (self, stock_info):
+		print ("")
+		# print (stock_info)
+		print ("Printing Live Quote...")
+		print ("")
+		print ("Name: ", stock_info['Name'])
+		print ("Symbol: ", stock_info["Symbol"])
+		print ("LastPrice: ", stock_info["LastPrice"])
+		print ("Timestamp: ", stock_info["Timestamp"])
+		print ("Open: ", stock_info["Open"])
+		print ("Change: ", stock_info["Change"])
+		print ("High: ", stock_info["High"])
+		print ("Low: ", stock_info["Low"])
+		print ("ChangeYTD: ", stock_info["ChangeYTD"])
+		print ("")
+
+	def print_portfolio (self, portfolio):
+		print ("Portfolio: ", portfolio)
+
+>>>>>>> local
